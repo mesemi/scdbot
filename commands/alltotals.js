855eb61f171e973e0ae1078ff1af6e4fbe092873
mesemi <mesemigame@gmail.com>
2021-08-08 22:55:04 -0400

    t

--- commands/alltotals.js
+++ commands/alltotals.js
@@ -1,13 +1,14 @@
 module.exports = {
-	name: 'ping',
-	description: 'Replies with Pong!',
+	name: 'alltotals',
+	description: 'Replies with all users totals.',
 	async execute(interaction) {
         const tests = require('./tests.json');
         const data = require('./data.json');
-		if (interaction.channel.id === 'admin-perms') {
+        const index = require('C:/Users/chad/OneDrive/Desktop/DISCORD BOTS/scdbot/index.js');
+		if (interaction.channel.id === '873711021330694214') {
 			var Content = 'Here is your status report:\n\n'
 			for (const [key, value] of Object.entries(data.users)) {
-				Content = Content + '<@' + key + '> has repaired `' + value.turbines + '` turbines and `' + value.hydrants + '` hydrants.\n'
+				Content = Content + '<@' + key + '> has gotten `' + value.testsDone + '` tests accepted.\n'
 			}
 			interaction.reply({content: Content, ephemeral: true})
 		} else {
